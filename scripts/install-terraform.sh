87ff266bd7442a4f8b414b09a8d7f7e53926c755
Randolfo Castillo <rcastillo@balanz.com>
2022-09-01 16:00:40 -0300

    feat: update tf / pb aws-cli

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -10,4 +10,5 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
     https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
     sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
+terraform -install-autocomplete
 terraform --version
