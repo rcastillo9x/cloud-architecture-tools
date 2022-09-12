a74579479b2e5936e426a4d7a634fa908828cc6b
Randolfo Castillo <rcastillo@balanz.com>
2022-09-12 15:29:07 -0300

    feat:add control tf

--- ws/control/main.tf
+++ ws/control/main.tf
@@ -42,10 +42,12 @@ data "aws_ssm_parameter" "ami" {
 
 # NETWORKING #
 resource "aws_vpc" "control_vpc" {
-  name = "test"
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
   tags = local.common_tags
+  tags {
+    name = join("-",[local.prefix.bu,local.prefix.env,local.prefix.vpc])
+  }
 }
 /*
 resource "aws_internet_gateway" "igw" {
