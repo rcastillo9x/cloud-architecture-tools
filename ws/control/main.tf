e7ac61f8843736f98a5b03da1551b7544082c37c
Randolfo Castillo <rcastillo@balanz.com>
2022-09-12 16:17:51 -0300

    feat:add control tf

--- ws/control/main.tf
+++ ws/control/main.tf
@@ -15,8 +15,7 @@
 ##################################################################################
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  shared_credentials_file = "~/.aws/credentials"
   region     = var.aws_region
 }
 
