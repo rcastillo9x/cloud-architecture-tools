c0707c8b8e00634c3077823436efd4d1a429614f
Randolfo Castillo <rcastillo@balanz.com>
2022-09-13 22:49:16 -0300

    feat: fix aws credentials

--- ws/control/variables.tf
+++ ws/control/variables.tf
@@ -2,18 +2,6 @@
 # TECH CONFIGURATION
 ##################################################################################
 
-variable "aws_access_key" {
-  type        = string
-  description = "AWS Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  type        = string
-  description = "AWS Secret Key"
-  sensitive   = true
-}
-
 variable "aws_region" {
   type        = string
   description = "Region for AWS Resources"
