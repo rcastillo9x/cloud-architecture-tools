dffe5dfe02fd2de985a517a3b7f5178cfc3446b2
Randolfo Castillo <rcastillo@balanz.com>
2022-09-13 23:32:48 -0300

    feat: aws

--- ws/control/main.tf
+++ ws/control/main.tf
@@ -15,7 +15,7 @@
 ##################################################################################
 
 provider "aws" {
-  shared_credentials_file = "~/.aws/credentials"
+  shared_credentials_files = "~/.aws/credentials"
   region     = var.aws_region
 }
 
